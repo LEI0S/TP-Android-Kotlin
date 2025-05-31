c0bc3791718827ab4033701b89d652dd0d034ef1
JuJu <julien.thepaut@epfedu.fr>
2025-05-31 13:26:28 +0200

    "Change DOC and README"

--- app/src/main/java/com/example/projet_androidkotlin/ui/screens/CartScreen.kt
+++ app/src/main/java/com/example/projet_androidkotlin/ui/screens/CartScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -19,7 +18,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
